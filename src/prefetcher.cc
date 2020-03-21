4b8390191bb645dee20bd8fed4879ba3e9d5b7d6
Solveig Skavnes <solvsk@ubuntu1804-02.idi.ntnu.no>
2020-03-21 20:16:50 +0100

    new functions implemented

--- src/prefetcher.cc
+++ src/prefetcher.cc
@@ -14,10 +14,13 @@
 using namespace std;
 typedef uint64 Delta;
 
+//Define:
+#MAX_NUM_OF_ENTRIES= 100;
+
 //Declarations:
 std::list <Addr> prefetches;
 std::list <Addr> candidates;
-
+deque<Addr> inFlight;
 
 //This is the struct that represents an enty in the DCPT table. Struct instead of class to have all member be public.
 struct entry{
@@ -57,15 +60,42 @@ entry *dcptable::find_entry (Addr pc){
 return NULL; //If no entry is found with the current PC
 }
 
-int DeltaCorrelation(entry entry){ //From paper on DCPT
-//TODO: fill inn this function acording to paper psaudocode 
-// (Delta ias typedefined at the top of this file!)
-return candidates[];
+void DeltaCorrelation(entry entry){ //From paper on DCPT
+    candidates.clear;
+    deque<Delta>::iterator del_it = entry->deltas.end(); //Create an delta iterator at the end of the delta deque
+    del_it -= 1; //push it back to the last entry
+    Delta last_delta = *del_it; 
+    del_it -= 1; //push it back ti the second to last entry
+    Delta 2_last_delta = *del_it;
+    Addr addr = entry->lastAddr; 
+    del_it = entry->detas.begin();
+    
+    while(del_it != entry->deltas.end()){
+        Delta temp1 = *del_it;
+        del_it++;
+        Delta temp2 = *del_it;
+        
+        if((temp1 == 2_last_delta) && (temp2 == last_delta)){ //If a pattern is recognized
+            for(del_it; del_it != entry->deltas.end(); del_it++){ //for each remaining delta
+                addr += del_it * BLOCK_SIZE;
+                candidates.pushback(addr);
+            }
+        }
+    }
 }
 
-int PrefetchFilter(entry entry, candidates[]){ //From paper on DCPT
-//TODO: fill inn this function acording to paper psaudocode
-return prefetches[];
+void PrefetchFilter(entry entry){ //From paper on DCPT
+    prefetches.clear();
+    list<Addr>::iterator cand_it; //Make a candidate iterator
+
+    for(cand_it = candidates.begin(); cand_it != candidates.end(); cand_it++){
+        // See if candidate is in inFlight or MSHR or cache
+        if("candidate is not present"){
+            prefetches.pushback(*cand_it);
+        }
+       inFlight.pushback(*cand_it); 
+       entry->lastPrefetch = cand_it;
+    }
 }
 
 void prefetch_access(AccessStat stat){ 
@@ -82,7 +112,7 @@ Addr pf_addr = stat.mem_addr + BLOCK_SIZE;
     entry *curr_e = table->find_entry(stat.pc); //See if the PC corresponds to an existing entry
     
     if (curr_e == NULL){  //make new entry 
-        if (dcptable.num_of_entries >= MAX_num_of_entries){
+        if (dcptable.num_of_entries >= Max_num_of_entries){
             // TODO: --> Remove oldest entry
         }
     curr_e.PC = stat.pc;
