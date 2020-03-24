698850d639f87e28f8744c0d15c9fc722563eded
Håvard O. Nordstrand <haavard@haavard.tech>
2020-03-24 11:21:12 +0100

    Optimize parameters a bit

--- src/prefetcher.hh
+++ src/prefetcher.hh
@@ -1,12 +1,12 @@
 #pragma once
 
-// Table size in number of entries (tunable)
-#define TABLE_SIZE 256
-// Number of delta values (tunable)
-#define DELTA_COUNT 16
-// Number of bits in each delta value (tunable)
-#define DELTA_WIDTH 16
 // Number of uncompleted prefetches to monitor (don't modify)
 #define MAX_UNCOMPLETED_PREFETCH 32
+// Table size in number of entries (tunable)
+#define TABLE_SIZE 128
+// Number of delta values (tunable)
+#define DELTA_COUNT 12
+// Number of bits in each delta value (tunable)
+#define DELTA_WIDTH 16
 
 typedef Addr BlockAddr;
