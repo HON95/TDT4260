e3194166a94d2b6d91bdf0f27c2cc81de1ee939f
Solveig Skavnes <solvsk@ubuntu1804-02.idi.ntnu.no>
2020-03-25 21:40:55 +0100

    optimized parameters for FOM

--- src/prefetcher.hh
+++ src/prefetcher.hh
@@ -3,9 +3,9 @@
 // Number of uncompleted prefetches to monitor (don't modify)
 #define MAX_UNCOMPLETED_PREFETCH 32
 // Table size in number of entries (tunable)
-#define TABLE_SIZE 128
+#define TABLE_SIZE 32
 // Number of delta values (tunable)
-#define DELTA_COUNT 12
+#define DELTA_COUNT 6
 // Number of bits in each delta value (tunable)
 #define DELTA_WIDTH 16
 
